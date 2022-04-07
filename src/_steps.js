422c873e6d20cefe833eb996f581102b5ddf9d87
AshrafulAlam <ashalam610@gmail.com>
2022-04-07 20:22:11 +0600

    conect-project-with-github

---
+++ src/_steps.js
@@ -0,0 +1,12 @@
+/* 
+  * Steps to use firebase
+    1 . Create a project on console.firebase.google.com 
+    2 . install firebase 
+    3 . Register web app in firebase
+    4 . copy firebase init with config from  firebase  project settings
+
+
+
+
+
+*/
---
+++ src/firebase.init.js
@@ -0,0 +1,13 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyB0PWFRELkHmPtETXG554GsuNWINGai2Zk",
+    authDomain: "simple-firebase-authenti-7eb61.firebaseapp.com",
+    projectId: "simple-firebase-authenti-7eb61",
+    storageBucket: "simple-firebase-authenti-7eb61.appspot.com",
+    messagingSenderId: "238724917744",
+    appId: "1:238724917744:web:94f3b334822f56f9525535"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+export default app 
